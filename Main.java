2e7d7ab0fb3c6a7ed3547235d127870115269747
Ishan Gandhi <ishangandhi@Ishans-MacBook-Pro.local>
2026-01-05 00:26:59 +0000

    starting on red node

--- Main.java
+++ Main.java
@@ -1,22 +1,42 @@
 import java.util.*;
 public class Main{
+
+    static void addRedToList(){
+        System.out.println("test");
+    }
+
     public static void main(String[] args){
-        ArrayList<Node> nodes = new ArrayList<Node>();
-        
+        Random r = new Random();
+
+        ArrayList<Node> allNodes = new ArrayList<Node>();
+        ArrayList<Node> greenNodeList = new ArrayList<Node>();
+        ArrayList<Node> redNodeList = new ArrayList<Node>();
+        ArrayList<Node> connectableNodes = new ArrayList<Node>();
+
         //Change this later to depend on easy, medium, hard
-        Integer greenNodes = 5;
+        Integer greenNodes = r.nextInt(4, 8);
 
         //Adding the green nodes
         for (int i = 0; i<greenNodes; i++){
-            nodes.add(new Node(true));
+            allNodes.add(new Node(true));
+            greenNodeList.add(new Node(true));
+            connectableNodes.add(new Node(true));
         }
+        //Removing last node from connectables
+        connectableNodes.remove(connectableNodes.getLast());
+
         //Adding the front connection
         for (int i = 0; i<greenNodes-1; i++){
-            nodes.get(i).con2 = nodes.get(i+1);
+            allNodes.get(i).con2 = allNodes.get(i+1);
         }
         //Adding the back connection
         for (int i = 1; i<greenNodes; i++){
-            nodes.get(i).con1 = nodes.get(i-1);
+            allNodes.get(i).con1 = allNodes.get(i-1);
         }
+        //Creating the loop that adds the red nodes
+        for (Node node : connectableNodes) {
+            System.out.println("test");
+        }
+
     }
 }
--- node.java
+++ node.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class Node{
     public Node con1;
